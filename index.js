685dde76b702cc833818d091f36eaf6245dc9926
envy69 <42788401+envy69@users.noreply.github.com>
2018-09-08 00:39:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("fuck js", {type: "WATCHING"});
+  bot.user.setActivity("myself", {type: "dicking"});
 
 });
 
